d06298b96ed4428b59ea52ab6b8dadba7eb077f4
Gabriel Gonzalez <gegb1994@gmail.com>
2019-04-30 23:50:14 -0400

    Added main menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
@@ -7,73 +8,87 @@ public class Main {
 
     public static void main(String[] args){
 
-        int inputSize  = 10240000;
-
-        System.out.println("TEST GIT");
-
-        long start = System.nanoTime();
         SkipList list = new SkipList();
-        ArrayList<Integer> itemsToSearch = new ArrayList<>();
-        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        boolean exitFlag = false;
 
 
-        int size= 0;
-        int y = 0;
+        while(!exitFlag) {
+            printMenu();
 
-        while(size != inputSize){
+            int choice = input.nextInt();
 
-            int randomNumber = random.nextInt(inputSize * 10);
-            list.insertElement(randomNumber, false);
-            size = list.getListSize();
-            if(y == 1000000) {
-                itemsToSearch.add(randomNumber);
-                y = 0;
+            switch (choice) {
+                case 1:
+                    System.out.println("Insert Element");
+                    break;
+                case 2:
+                    System.out.println("Remove Element");
+                    break;
+                case 3:
+                    System.out.println("Print List");
+                    break;
+                case 4:
+                    System.out.println("Closest Key After");
+                    break;
+                case 5:
+                    exitFlag = true;
+                    break;
+                default:
+                    printMenu();
             }
-            y++;
-
-        }
-
-        long end = System.nanoTime();
-        long total = end - start;
-        System.out.println("Time taken for insert: " + total);
-
-        System.out.println(list.getListSize());
-
-        for(Integer i:itemsToSearch){
-            list.findElement(i, true);
         }
 
 
 
 
+//        int inputSize  = 10240000;
+//
+//        System.out.println("TEST GIT");
+//
+//        long start = System.nanoTime();
+//        SkipList list = new SkipList();
+//        ArrayList<Integer> itemsToSearch = new ArrayList<>();
+//        Random random = new Random();
+//
+//
+//        int size= 0;
+//        int y = 0;
+//
+//        while(size != inputSize){
+//
+//            int randomNumber = random.nextInt(inputSize * 10);
+//            list.insertElement(randomNumber, false);
+//            size = list.getListSize();
+//            if(y == 1000000) {
+//                itemsToSearch.add(randomNumber);
+//                y = 0;
+//            }
+//            y++;
+//
+//        }
+//
+//        long end = System.nanoTime();
+//        long total = end - start;
+//        System.out.println("Time taken for insert: " + total);
+//
+//        System.out.println(list.getListSize());
+//
+//        for(Integer i:itemsToSearch){
+//            list.findElement(i, true);
+//        }
+//
+    }
 
-//        list.printList();
+    private static void printMenu(){
 
-//        list.insertElement(3 );
-//        list.insertElement(10 );
-//        list.insertElement(15 );
-//        list.insertElement(13);
-//        list.insertElement(7);
-//        list.insertElement(19);
-//        list.insertElement(25);
-//        list.insertElement(4);
-//        list.insertElement(4);
-//        list.insertElement(1);
-//        list.insertElement(5);
-//        list.insertElement(30);
-////        System.out.println(list.getHead().getKey() + " " + list.getHead().getNext().getKey());
-//        list.printList();
-//        System.out.println("after removal");
-//        list.removeElement(10);
-//        list.removeElement(1);
-//        list.removeElement(2);
-//        list.removeElement(3);
-//        list.printList();
-//        list.insertElement(10);
-//        list.printList();
-//        int closest = list.closestKeyAfter(5);
-//        System.out.println("Closest key after 3 is: " + closest);
-
+        System.out.println("[1] Insert Element");
+        System.out.println("[2] Remove Element");
+        System.out.println("[3] Print List");
+        System.out.println("[4] Closest Key After");
+        System.out.println("[5] Exit");
+        System.out.print(">");
 
 
     }
