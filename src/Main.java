cecb440dd85e180b95ce8334b90c5283b26ddba4
Gabriel Gonzalez <gegb1994@gmail.com>
2019-05-03 12:58:47 -0400

    final version

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,12 @@ public class Main {
     public static List<Integer> itemsForRemoval = null;
     public static List<Integer> itemsToInsert = null;
 
+    public static List<Long> finalAverageSearchTime = null;
+    public static List<Long> finalAverageFailedSearchTime = null;
+    public static List<Long> finalAverageClosestKeyAfterTime = null;
+    public static List<Long> finalAverageRemovalTime = null;
+    public static List<Long> finalAverageInsertTime = null;
+
 
     public static void main(String[] args) {
 
@@ -53,10 +59,6 @@ public class Main {
                         findMenu(list);
                         break;
                     case 6:
-                        list = new SkipList();
-                        generateRandomList(list);
-                        break;
-                    case 7:
                         exitFlag = true;
                         break;
                     default:
@@ -66,150 +68,128 @@ public class Main {
 
         } else {
 
-            int analysisSize;
+            int analysisSize, iterations;
             long generateTime;
             long averageSearchTime, averageFailedSearchTime, averageClosestAfterTime, averageRemovalTime, averageInsertionTime;
             long startTime, endTime, total;
 
-            System.out.println("Select size for statistics:");
-            System.out.println("[1] 128");
-            System.out.println("[2] 512");
-            System.out.println("[3] 1024");
-            System.out.println("[4] 2048");
-            System.out.println("[5] 4096");
-            System.out.println("[6] 8192");
-            System.out.println("[7] 16384");
-            System.out.println("[8] 32768");
-            System.out.println("[9] 65536");
-            System.out.println("[0] 131072");
+            System.out.println("Input size for complexity analysis:");
+            analysisSize = input.nextInt();
+            System.out.println("Input size for number of iterations for accuracy:");
+            iterations = input.nextInt();
 
-            int analysisChoice = input.nextInt();
-            switch (analysisChoice) {
+            finalAverageSearchTime = new ArrayList<>();
+            finalAverageInsertTime = new ArrayList<>();
+            finalAverageRemovalTime = new ArrayList<>();
+            finalAverageClosestKeyAfterTime = new ArrayList<>();
+            finalAverageFailedSearchTime = new ArrayList<>();
 
-                case 1:
-                    analysisSize = 128;
-                    break;
-                case 2:
-                    analysisSize = 512;
-                    break;
-                case 3:
-                    analysisSize = 1024;
-                    break;
-                case 4:
-                    analysisSize = 2048;
-                    break;
-                case 5:
-                    analysisSize = 4096;
-                    break;
-                case 6:
-                    analysisSize = 8192;
-                    break;
-                case 7:
-                    analysisSize = 16384;
-                    break;
-                case 8:
-                    analysisSize = 32768;
-                    break;
-                case 9:
-                    analysisSize = 65536;
-                    break;
-                case 0:
-                    analysisSize = 131072;
-                    break;
-                default:
-                    analysisSize = 0;
+            for(int i=0; i<iterations; i++) {
+                try {
+
+                    list = new SkipList();
+                    generateTime = generateList(list, analysisSize);
+                    System.out.println("Time taken to generate list: " + generateTime + " nanoseconds.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                System.out.println("Starting successful search operations...");
+
+                List<Long> average = new ArrayList<>();
+
+                for (int x : itemsToSearch) {
+                    startTime = System.nanoTime();
+                    list.findElement(x, true);
+                    endTime = System.nanoTime();
+                    total = endTime - startTime;
+                    average.add(total);
+                }
+
+                averageSearchTime = findAverage(average);
+
+
+                finalAverageSearchTime.add(averageSearchTime);
+
+//                System.out.println("Average time taken for 25 searches: " + averageSearchTime + " nanoseconds");
+
+                System.out.println("Starting failed search operations...");
+
+                List<Long> averageFailed = new ArrayList<>();
+
+                for (int x : itemsForFailedSearch) {
+                    startTime = System.nanoTime();
+                    list.findElement(x, true);
+                    endTime = System.nanoTime();
+                    total = endTime - startTime;
+                    averageFailed.add(total);
+                }
+
+                averageFailedSearchTime = findAverage(averageFailed);
+
+                finalAverageFailedSearchTime.add(averageFailedSearchTime);
+
+//                System.out.println("Average time taken for 25 failed searches: " + averageFailedSearchTime + " nanoseconds");
+
+                System.out.println("Starting closest key after operations...");
+
+                List<Long> averageClosest = new ArrayList<>();
+
+                for (int x : itemsForClosestKeyAfter) {
+                    long totalTime = list.closestKeyAfter(x);
+                    averageClosest.add(totalTime);
+                }
+
+                averageClosestAfterTime = findAverage(averageClosest);
+                finalAverageClosestKeyAfterTime.add(averageClosestAfterTime);
+
+//                System.out.println("Average time taken for 50 closest key after operations is: " + averageClosestAfterTime + " nanoseconds");
+
+                System.out.println("Starting removal operations...");
+
+                List<Long> averageRemoval = new ArrayList<>();
+
+                for (int x : itemsForRemoval) {
+                    long totalRemovalTime = list.removeElement(x);
+                    averageRemoval.add(totalRemovalTime);
+                    list.insertElement(x, false);
+                }
+
+                averageRemovalTime = findAverage(averageRemoval);
+                finalAverageRemovalTime.add(averageRemovalTime);
+
+//                System.out.println("Average time taken for 50 removal key operations is: " + averageRemovalTime + " nanoseconds");
+
+                System.out.println("Starting inserting key operations...");
+
+                List<Long> averageInsert = new ArrayList<>();
+
+                for (int x : itemsToInsert) {
+                    long insertTime = list.insertElement(x, true);
+                    averageInsert.add(insertTime);
+                    list.removeElement(x);
+                }
+
+                averageInsertionTime = findAverage(averageInsert);
+                finalAverageInsertTime.add(averageInsertionTime);
+//                System.out.println("Average time taken for 50 insert operations is: " + averageInsertionTime + " nanoseconds");
             }
 
-            try {
-                generateTime = generateList(list, analysisSize);
-                System.out.println("Time taken to generate list: " + generateTime + " nanoseconds.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
-
-            System.out.println("Starting successful search operations...");
-
-            List<Long> average = new ArrayList<>();
-
-            for (int x : itemsToSearch) {
-                startTime = System.nanoTime();
-                list.findElement(x, true);
-                endTime = System.nanoTime();
-                total = endTime - startTime;
-                average.add(total);
-            }
-
-            averageSearchTime = findAverage(average);
-
-            System.out.println("Average time taken for 25 searches: " + averageSearchTime + " nanoseconds");
-
-            System.out.println("Starting failed search operations...");
-
-            List<Long> averageFailed = new ArrayList<>();
-
-            for (int x : itemsForFailedSearch) {
-                startTime = System.nanoTime();
-                list.findElement(x, true);
-                endTime = System.nanoTime();
-                total = endTime - startTime;
-                averageFailed.add(total);
-            }
-
-            averageFailedSearchTime = findAverage(averageFailed);
-
-            System.out.println("Average time taken for 25 failed searches: " + averageFailedSearchTime + " nanoseconds");
-
-            System.out.println("Starting closest key after operations...");
-
-            List<Long> averageClosest = new ArrayList<>();
-
-            for (int x : itemsForClosestKeyAfter) {
-                long totalTime = list.closestKeyAfter(x);
-                averageClosest.add(totalTime);
-            }
-
-            averageClosestAfterTime = findAverage(averageClosest);
-
-            System.out.println("Average time taken for 50 closest key after operations is: " + averageClosestAfterTime + " nanoseconds");
-
-            System.out.println("Starting removal operations...");
-
-            List<Long> averageRemoval = new ArrayList<>();
-
-            for(int x : itemsForRemoval){
-                long totalRemovalTime = list.removeElement(x);
-                averageRemoval.add(totalRemovalTime);
-            }
-
-            averageRemovalTime = findAverage(averageRemoval);
-
-            System.out.println("Average time taken for 50 removal key operations is: " + averageRemovalTime + " nanoseconds");
-
-            System.out.println("Starting inserting key operations...");
-
-            List<Long> averageInsert = new ArrayList<>();
-
-            for(int x : itemsToInsert){
-                long insertTime = list.insertElement(x, true);
-                averageInsert.add(insertTime);
-            }
-
-            averageInsertionTime = findAverage(averageInsert);
-            System.out.println("Average time taken for 50 insert operations is: " + averageInsertionTime + " nanoseconds");
             System.out.println("\n");
             System.out.println("FINAL RESULTS FOR LIST SIZE " + analysisSize);
             System.out.println("                    | AVERAGE NANOSECONDS");
             System.out.println("-----------------------------------------");
-            System.out.println("SEARCH              | "+ averageSearchTime);
+            System.out.println("SEARCH              | "+ findAverage(finalAverageSearchTime));
             System.out.println("-----------------------------------------");
-            System.out.println("FAILED SEARCH       | "+ averageFailedSearchTime);
+            System.out.println("FAILED SEARCH       | "+ findAverage(finalAverageFailedSearchTime));
             System.out.println("-----------------------------------------");
-            System.out.println("CLOSEST KEY AFTER   | "+ averageClosestAfterTime);
+            System.out.println("CLOSEST KEY AFTER   | "+ findAverage(finalAverageClosestKeyAfterTime));
             System.out.println("-----------------------------------------");
-            System.out.println("REMOVAL             | "+ averageRemovalTime);
+            System.out.println("REMOVAL             | "+ findAverage(finalAverageRemovalTime));
             System.out.println("-----------------------------------------");
-            System.out.println("INSERT              | "+ averageInsertionTime);
+            System.out.println("INSERT              | "+ findAverage(finalAverageInsertTime));
             System.out.println("-----------------------------------------");
 
 
@@ -233,25 +213,6 @@ public class Main {
 
     }
 
-    private static void generateRandomList(SkipList list) {
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("How many items: (Range 1-50: PRINT / Range 50+: NO PRINT)");
-        int desiredSize = input.nextInt();
-
-        try {
-            generateList(list, desiredSize);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (desiredSize <= 50)
-            list.printList();
-
-
-    }
-
     private static long generateList(SkipList list, int desiredSize) throws IOException {
 
         Random random = new Random();
@@ -264,7 +225,7 @@ public class Main {
         long start = System.nanoTime();
 
         //itemSpread is used to capture 50 items while list is getting created
-        int itemSpread = desiredSize / 25;
+        int itemSpread = desiredSize / 50;
         int itemSpread2 = desiredSize / 50;
         int itemSpread3 = desiredSize / 50;
 
@@ -273,14 +234,14 @@ public class Main {
         int closestSearchSize = 0;
         int removalSize = 1;
 
-        for (int i = 0; i < 25; i++) {
+        for (int i = 0; i < 50; i++) {
             itemsForFailedSearch.add(random.nextInt(desiredSize * 2));
         }
 
         int y = 0;
         while(y != 50){
             int key = random.nextInt(desiredSize * 2);
-            if(itemsForFailedSearch.contains(key))
+            if(itemsForFailedSearch.contains(key) || itemsToInsert.contains(key))
                continue;
             else {
                 itemsToInsert.add(key);
@@ -445,8 +406,8 @@ public class Main {
         System.out.println("[3] Print List");
         System.out.println("[4] Closest Key After");
         System.out.println("[5] Find Element");
-        System.out.println("[6] Generate Random List");
-        System.out.println("[7] Exit");
+//        System.out.println("[6] Generate Random List");
+        System.out.println("[6] Exit");
         System.out.print(">");
 
 
